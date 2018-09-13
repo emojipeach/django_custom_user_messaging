fc0cd851fbf7fbd91b859a549fb54201e4cdfbe4
emojipeach <37852121+emojipeach@users.noreply.github.com>
2018-09-13 22:53:05 +0000

    Create users/views.py

---
+++ users/views.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
+from django.contrib.auth import login
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.urls import reverse
+
+from users.forms import CustomUserCreationForm
+
+
+User = get_user_model()
+
+
+def signup(request):
+    """Register a new user."""
+    if request.method != 'POST':
+        # Display a blank registration form
+        form = CustomUserCreationForm()
+    else:
+        # Process completed form
+        form = CustomUserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            # log the user in then redirect to home page
+            authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse_lazy('index'))
+    context = {'form': form}
+    return render(request, 'users/signup.html', context)
+
+
+@login_required
+def password_change(request):
+    """ Allows a user to change their own password."""
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return HttpResponseRedirect(reverse('imageapp:profile', args=[request.user.username]))
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'users/password_change.html', context)
+
+
+def make_admin_view(request):
+    User.objects.create_superuser(username='admin', email='angelinvestor@coffeehouse.com', password='people666')
+    return HttpResponseRedirect(reverse_lazy('index'))
