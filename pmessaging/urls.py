d3929dfca3600b7d66ff090b1a0ab6aa3a5c2b7f
emojipeach <37852121+emojipeach@users.noreply.github.com>
2018-09-16 20:13:37 +0000

    Create urls.py

---
+++ pmessaging/urls.py
@@ -0,0 +1,73 @@
+from django.urls import path
+from django.urls import re_path
+from django.views.generic import RedirectView
+
+from . import views
+
+urlpatterns = [
+    # redirect to inbox
+    path(
+    '',
+    RedirectView.as_view(permanent=True, url='inbox/'),
+    name='messages_redirect'
+    ),
+    # inbox
+    path(
+        'inbox/',
+        views.inbox,
+        name='messages_inbox'
+        ),
+    # outbox
+    path(
+        'outbox/',
+        views.outbox,
+        name='messages_outbox'
+        ),
+    # compose
+    path(
+        'compose/',
+        views.compose,
+        name='messages_compose'
+        ),
+    # compose to username from url
+    path(
+        'compose/<str:recipient>/',
+        views.compose,
+        name='messages_compose_to'
+        ),
+    #re_path(
+        #r'^compose/(?P<recipient>[\w.@+-]+)/$',
+        #views.compose,
+        #name='messages_compose_to'
+        #),
+    # reply
+    path(
+        'reply/<slug:message_id>/',
+        views.reply,
+        name='messages_reply'
+        ),
+    # view
+    path(
+        'view/<slug:message_id>/',
+        views.view,
+        name='messages_detail'
+        ),
+    # delete
+    path(
+        'delete/<slug:message_id>/',
+        views.delete,
+        name='messages_delete'
+        ),
+    # undelete
+    path(
+        'undelete/<slug:message_id>/',
+        views.undelete,
+        name='messages_undelete'
+        ),
+    # trash
+    path(
+        'trash/',
+        views.trash,
+        name='messages_trash'
+        ),
+    ]
