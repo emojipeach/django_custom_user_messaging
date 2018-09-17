f3cddb2a0827475f014910edcd698203f3acce74
emojipeach <37852121+emojipeach@users.noreply.github.com>
2018-09-17 19:16:45 +0000

    Update models.py

--- users/models.py
+++ users/models.py
@@ -2,11 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 
 class CustomUser(AbstractUser):
-    lowercase_username = models.CharField(max_length=150, blank=True, null=True)
 
     def __str__(self):
         return self.username
