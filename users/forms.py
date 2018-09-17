4ac7a7bcd47eceb1531e4b4d9a5a8b99bc083656
emojipeach <37852121+emojipeach@users.noreply.github.com>
2018-09-17 00:06:49 +0000

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,22 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import UserChangeForm
+
 
 User = get_user_model()
 
 
 class CustomUserCreationForm(UserCreationForm):
+    
+    def unique_username(self):
+        username = self.cleaned_data.get('username').lower()
+        if User.objects.filter(lowercase_username=username):
+            raise forms.ValidationError(_('A user with that username already exists.'))
+        else:
+            return username
 
     class Meta(UserCreationForm.Meta):
         model = User
